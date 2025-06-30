37a69073e2b97cef120483f530adc6c4a4a8e266
ossilv <vohoanganhtruong@gmail.com>
2025-06-30 13:43:58 +0700

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(2, 3));
+        System.out.println(calculator.subtract(2, 3));
+        System.out.println(calculator.divide(2, 3));
+        System.out.println(calculator.multiply(2, 3));
+    }
+}
