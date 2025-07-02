b96fdb5bc51426c82c0d143bb16b4df45f21c731
ossilv <vohoanganhtruong@gmail.com>
2025-07-02 16:41:35 +0700

    [dev]: change the output

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,9 +6,9 @@ public class App
     {
         Calculator calculator = new Calculator();
 
-        System.out.println(calculator.add(2, 3));
-        System.out.println(calculator.subtract(2, 3));
-        System.out.println(calculator.divide(2, 3));
-        System.out.println(calculator.multiply(2, 3));
+        System.out.println("2 + 3 = " + calculator.add(2, 3));
+        System.out.println("2 - 3 = " + calculator.subtract(2, 3));
+        System.out.println("2 / 3 = " + calculator.divide(2, 3));
+        System.out.println("2 * 3 = " + calculator.multiply(2, 3));
     }
 }
